9e44830cf0e49495413ffb61fc043816a3e33e82
haggai.eran <haggai.eran@bd8d0ae3-1ec3-96b9-7fbc-0d4ed58d5751>
2010-06-03 09:05:13 +0000

    Added an automaton class that links with soot.

---
+++ soot.typestate/src/soot/typestate/automata/tests/Automaton.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package soot.typestate.automata.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import soot.SootMethod;
+import soot.typestate.automata.AutomataStandaloneSetup;
+import soot.typestate.automata.ClassAutomaton;
+
+/**
+ * @author haggai
+ *
+ */
+public class Automaton {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		AutomataStandaloneSetup.doSetup();
+	}
+
+	/**
+	 * Test method for {@link soot.typestate.automata.ClassAutomaton#ClassAutomaton(soot.typestate.automata.automata.Automaton, java.lang.Package)}.
+	 * @throws Exception 
+	 */
+	@Test
+	public void testClassAutomaton() throws Exception {
+		ClassAutomaton listAutomaton = ClassAutomaton.load("examples/java.util.Collection.automata");
+		SootMethod method = listAutomaton.getKlass().getMethod("boolean add(java.lang.Object)");
+		assertEquals("NON_EMPTY", listAutomaton.getDelta(method, "NON_EMPTY"));
+		assertEquals("NON_EMPTY", listAutomaton.getDelta(method, "EMPTY"));
+		method = listAutomaton.getKlass().getMethod("void clear()");
+		assertEquals("EMPTY", listAutomaton.getDelta(method, "NON_EMPTY"));
+		assertEquals("EMPTY", listAutomaton.getDelta(method, "EMPTY"));
+	}
+
+}
