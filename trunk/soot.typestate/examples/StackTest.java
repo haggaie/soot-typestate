7f8a16e8a9709aa922feba29a6b7485a8003e14e
haggai.eran <haggai.eran@bd8d0ae3-1ec3-96b9-7fbc-0d4ed58d5751>
2010-06-04 06:13:02 +0000

    A java.util.Stack automaton and example code. Its better than the Collection since it has error conditions.

---
+++ trunk/soot.typestate/examples/StackTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StackTest {
+
+	Stack<Integer> stack;
+	
+	@Before
+	public void setUp() throws Exception {
+		stack = new Stack<Integer>();
+	}
+
+	@Test
+	public void testPush() {
+		stack.push(1);
+		stack.pop();
+	}
+
+	@Test(expected = EmptyStackException.class)
+	public void testPop() {
+		stack.pop();
+	}
+
+	@Test
+	public void testEmpty() {
+		assertTrue(stack.empty());
+		stack.push(1);
+		stack.clear();
+		assertTrue(stack.empty());
+	}
+
+	@Test(expected = EmptyStackException.class)
+	public void testClear() {
+		// Creating a new stack to have a simpler
+		// example where the stack is allocated locally.
+		Stack<Integer> s = new Stack<Integer>();
+		s.push(1);
+		s.clear();
+		s.pop();
+	}
+
+}
