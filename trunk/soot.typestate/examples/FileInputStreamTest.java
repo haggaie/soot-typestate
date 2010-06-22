e42e754a1f11e8124bd6ee7b505b604be1ce69b5
shakedt2 <shakedt2@bd8d0ae3-1ec3-96b9-7fbc-0d4ed58d5751>
2010-06-22 11:05:51 +0000

    FileInputStream example automaton.

---
+++ trunk/soot.typestate/examples/FileInputStreamTest.java
@@ -0,0 +1,75 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+
+public class FileInputStreamTest {
+	
+	public void test1() throws IOException
+	{
+		FileInputStream file = new FileInputStream("the_file_name");
+		file.read();
+		file.close();
+	}
+	
+	public void test2() throws IOException
+	{
+		FileInputStream file = new FileInputStream("the_file_name");
+		file.close();
+		file.read();
+	}
+	
+	public void test3() throws IOException
+	{
+		FileInputStream file1 = new FileInputStream("the_file_name");
+		file1.read();
+		file1.close();
+		
+		FileInputStream file2 = new FileInputStream("the_file_name");
+		file2.read();
+		file2.close();
+	}
+	
+	public void test4() throws IOException
+	{
+		FileInputStream file = new FileInputStream("the_file_name");
+		file.read();
+		file.close();
+		
+		file = new FileInputStream("the_file_name");
+		file.read();
+		file.close();
+	}
+	
+	public void test5() throws IOException
+	{
+		FileInputStream file = new FileInputStream("the_file_name");
+		for(int c=0; c < 100; ++c)
+			file.read();
+		
+		file.close();
+	}
+	
+	public void test6() throws IOException
+	{
+		for(int c=0; c < 100; ++c)
+		{
+			FileInputStream file = new FileInputStream("the_file_name");
+			file.read();
+			file.close();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		FileInputStreamTest test = new FileInputStreamTest();
+		test.test1();
+		test.test2();
+		test.test3();
+		test.test4();
+		test.test5();
+		test.test6();
+	}
+}
+
